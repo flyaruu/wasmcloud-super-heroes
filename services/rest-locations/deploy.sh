e86d6ddb106c59c9f674ef80851b18030a4e16b1
Frank Lyaruu <flyaruu@gmail.com>
2025-05-02 16:21:14 +0200

    add initial location service

---
+++ services/rest-locations/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+export SERVICE_NAME=rest-locations
+export BINARY_NAME=rest_locations
+echo "Deploying $SERVICE_NAME"
+
+echo "Deploying $SERVICE_NAME"
+wash build
+wash push --insecure localhost:5001/superheroes/${SERVICE_NAME}:0.1.0 build/${BINARY_NAME}_s.wasm
+wash app deploy --replace wadm.yaml
