ad211f4ab9ca580578ccdea16e2e6a0e4196245f
Frank Lyaruu <flyaruu@gmail.com>
2025-05-07 17:52:20 +0200

    relax thresholds

--- k6/none.js
+++ k6/none.js
@@ -4,7 +4,7 @@ import { randomFight } from './randomFight.js';
 
 export const options = {
   thresholds: {
-    http_req_duration: ['p(95)<50'],  // 95% of requests should be <50ms
+    http_req_duration: ['p(95)<500'],  // 95% of requests should be <50ms
     http_req_failed: ['rate<0.001'],    // error rate should be <0.1%
     // dropped_iterations: ['count == 0'],  // no dropped iterations allowed
   },
