15164d9223faed988f83114fcf67b11dcf213a1f
Frank Lyaruu <flyaruu@gmail.com>
2025-05-07 14:42:40 +0200

    moved sh file

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+wash start provider ghcr.io/wasmcloud/sqldb-postgres:0.9.0 postgres-provider --ctl-host nats
+set -e
+cd services/rest-heroes
+./build.sh
+cd ..
+cd rest-villains
+./build.sh
+cd ..
+cd rest-locations
+./build.sh
+cd ..
+cd rest-fights
+./build.sh
+cd ../..
+
+# curl wasmcloud:8000/api/fights/randomfight
