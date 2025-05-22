e7d7be762a99eacf18fb9041f6ad3b5f7887c344
Frank Lyaruu <flyaruu@gmail.com>
2025-05-22 12:06:45 +0200

    fix deploy script

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-set -e
 wash start provider ghcr.io/wasmcloud/sqldb-postgres:0.9.0 postgres-provider
-cd services/rest-heroes
-./deploy.sh
-cd ../..
-cd services/rest-villains
-./deploy.sh
-cd ../..
-cd services/rest-locations
-./deploy.sh
-cd ../..
-cd services/rest-fights
-./deploy.sh
-cd ../..
+set -e
 
+for dir in services/*/; do
+  if [ -x "$dir/deploy.sh" ]; then
+    (cd "$dir" && ./deploy.sh)
+  fi
+done
+
+wash app deploy --replace ./services/wadm.yaml
