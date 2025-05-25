7b243724ccf5c539af2b9dec204f2c513a3c5dd9
Frank Lyaruu <flyaruu@gmail.com>
2025-05-25 18:59:11 +0200

    demand no dropped http

--- k6/load.js
+++ k6/load.js
@@ -4,9 +4,9 @@ import { randomFight } from './randomFight.js';
 
 export const options = {
   thresholds: {
-    // http_req_duration: ['p(95)<500'],  // 95% of requests should be <50ms
+    http_req_duration: ['p(95)<500'],  // 95% of requests should be <50ms
     http_req_failed: ['rate<0.001'],    // error rate should be <0.1%
-    // dropped_iterations: ['count == 0'],  // no dropped iterations allowed
+    dropped_iterations: ['count == 0'],  // no dropped iterations allowed
   },
   scenarios: {
     ramp_high_load: {
