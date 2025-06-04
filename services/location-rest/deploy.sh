c18d27e76caa7455c27110e4741c8ca8ef049993
Frank Lyaruu <flyaruu@gmail.com>
2025-06-04 23:31:30 +0200

    unify cardamon with other impls

--- services/location-rest/deploy.sh
+++ services/location-rest/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-set -e
+set -e -x
 export SERVICE_NAME=location-rest
 export BINARY_NAME=location_rest
 WASMCLOUD_NATS_HOST=${WASMCLOUD_NATS_HOST:-"localhost"}
+export WASM_REGISTRY_BASE_URL=${1:-'localhost:5001/superheroes'}
 
 echo "Deploying $SERVICE_NAME"
 wash build
